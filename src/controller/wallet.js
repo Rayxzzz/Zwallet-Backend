9efb595621a8f2a70c0aac97071375ab08ce7375
Rayzxxx <ridhosuhendar1@gmail.com>
2022-01-14 22:33:20 +0700

    refactoring

--- src/controller/wallet.js
+++ src/controller/wallet.js
@@ -1,12 +1,16 @@
 const modelWallet = require('../models/wallet')
 const responsStandart = require('../helper/common')
+const userModel = require('../models/user')
+
 
 // eslint-disable-next-line no-unused-vars
 const topUp = async (req, res, next) => {
   const idUser = req.params.id
   const balance = req.body.balance
+  const test = await userModel.readBalance(idUser)
+  console.log(test[0].balance)
   const data = {
-    balance: data.balance + balance
+    balance: test[0].balance + balance 
   }
 
   // eslint-disable-next-line no-unused-vars
