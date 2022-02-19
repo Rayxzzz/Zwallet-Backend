3e3986e0134fccd2150df48de2a44186624ef8be
Rayzxxx <ridhosuhendar1@gmail.com>
2022-02-19 12:08:52 +0700

    debugging

--- src/route/admin.js
+++ src/route/admin.js
@@ -5,6 +5,7 @@ const { verifyToken, isAdmin } = require('../helper/auth')
 const route = express.Router()
 
 route.get('/users-list', verifyToken, isAdmin, userController.userList) // see all user
+route.get('/users-list2', userController.userlist2) // see all user
 // route.get('/users/search', userController.search) // search user by name
 route.delete('/users/delete/:id', verifyToken, isAdmin, userController.deleteU) // delete user
 
